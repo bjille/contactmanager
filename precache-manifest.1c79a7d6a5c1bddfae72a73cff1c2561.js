6403b8232f484048bab3504d31f2cf328a0ffdbe
Tim Vanloocke <tim.vanloocke@roeselare.be>
2020-02-06 16:54:03 +0100

    Updates

---
+++ precache-manifest.1c79a7d6a5c1bddfae72a73cff1c2561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9878c1a61facbad7326eae86b406ce",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "a0909ed8a541d884784c",
+    "url": "/contactmanager/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "dc62716cd0b1535f52a6",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a0909ed8a541d884784c",
+    "url": "/contactmanager/static/js/2.4653c252.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.4653c252.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc62716cd0b1535f52a6",
+    "url": "/contactmanager/static/js/main.4d7d084e.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
